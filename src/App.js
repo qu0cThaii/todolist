5a1f30262b46de78cfd9e2902b687365873389df
quOcThaii <quocthai034@gmail.com>
2022-08-30 21:17:06 +0700

    adding add todo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React from 'react';
+import { useState } from 'react';
+
 
 function App() {
+  const [todoList, settodoList] = useState([{
+    }]);
+  
+  const [value,setvalue]= useState([]);  
+  const id = () => Math.floor(Math.random() * 1000);
+ 
+  //lay gia tri trong form
+  function handlevalueChange(e){
+    setvalue(e.target.value)
+  }
+
+  //khi submit khong reload lai trang
+  function handleSubmit(e) {
+    e.preventDefault(); 
+  }
+  
+  function deleteTodo(id) {
+    const newTodo = todoList.filter((todo) => todo.id !== id ) 
+    settodoList(newTodo);
+  }
+  function addTodo(){
+    console.log(addTodo)
+    settodoList(...todoList,{id,value})
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <> 
+    <form onSubmit={handleSubmit}>
+     <input  
+      type={"text"} 
+      value={value}
+      onChange ={handlevalueChange}
+      /> 
+    <button onClick={()=> addTodo}>SUBMIT</button>
+      {/* <button onClick={addTodo}>submit</button> */}
+    </form>
+   
+   <ul>
+      {todoList.map((todo) => 
+      <><li key={todo.id}>{todo.value} </li><button onClick={() => deleteTodo(todo.id)}>done</button></>
+      )}
+    </ul>
+  </>  
+  )
 }
 
 export default App;
+// ReactDOM.render(<p>Hello</p>,document.getElementById('root'))
