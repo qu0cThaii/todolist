1fd8b417d656cbe36e84b8807a364e8599b98be1
quOcThaii <quocthai034@gmail.com>
2022-09-01 23:48:38 +0700

    add todo

--- src/App.js
+++ src/App.js
@@ -5,50 +5,49 @@ import { useState } from 'react';
 
 
 function App() {
-  const [todoList, settodoList] = useState([{
-    }]);
-  
-  const [value,setvalue]= useState([]);  
-  const id = () => Math.floor(Math.random() * 1000);
+  const id = () => Math.floor(Math.random()*100);
+  const [todoList, settodoList] = useState([
+    
+]);
  
-  //lay gia tri trong form
-  function handlevalueChange(e){
-    setvalue(e.target.value)
-  }
+  const [newitem,setnewitem]= useState();
 
-  //khi submit khong reload lai trang
-  function handleSubmit(e) {
+   function handlesubmit (e) {
     e.preventDefault(); 
-  }
+    const newvalue = [...todoList,{id,title:newitem}]
+    settodoList(newvalue)
+   }
   
-  function deleteTodo(id) {
-    const newTodo = todoList.filter((todo) => todo.id !== id ) 
-    settodoList(newTodo);
-  }
-  function addTodo(){
-    console.log(addTodo)
-    settodoList(...todoList,{id,value})
-
+  function handlechange(e) {
+    return setnewitem(e.target.value);
   }
 
+    //reset sau khi nhap form
+    
+    function deletetodo(id){
+    const newtodo = todoList.filter(todo => todo.id !== id)
+    settodoList(newtodo)
+  }
   return (
-  <> 
-    <form onSubmit={handleSubmit}>
-     <input  
-      type={"text"} 
-      value={value}
-      onChange ={handlevalueChange}
-      /> 
-    <button onClick={()=> addTodo}>SUBMIT</button>
-      {/* <button onClick={addTodo}>submit</button> */}
+  <>
+    <form onSubmit={handlesubmit}>
+      <label>Hom nay ban can lam gi?
+      <br></br>
+      <input type={"text"} value={newitem} onChange={handlechange}></input>
+      <input type={"submit"}></input>
+      </label>
     </form>
-   
-   <ul>
-      {todoList.map((todo) => 
-      <><li key={todo.id}>{todo.value} </li><button onClick={() => deleteTodo(todo.id)}>done</button></>
+    
+    <>
+      <ul>
+      {todoList.map(todo=> 
+      <><li key={todo.id}>{todo.title}</li><button onClick={() => deletetodo(todo.id)}>hoan thanh</button></>
       )}
-    </ul>
-  </>  
+      
+      </ul>
+    
+    </>    
+</>
   )
 }
 
